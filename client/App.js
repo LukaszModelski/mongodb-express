3b9b59477f6b7234fc0fe0ed1f73653bd2f476f9
Czapa <lukaszmodelski90@gmail.com>
2020-11-29 13:53:16 +0100

    expo init React Native app

---
+++ client/App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+
+  useEffect(() => {
+    console.log('Log from useEffect');
+  });
+
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!!</Text>
+      <Text>Test3</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});

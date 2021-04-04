d869d1f38b5e50699765111eced556ae16ada768
Czapa <lukaszmodelski90@gmail.com>
2021-04-04 16:46:51 +0200

    Created user login route

--- server/src/resources/user/user.router.js
+++ server/src/resources/user/user.router.js
@@ -7,7 +7,7 @@ import { validateEmail } from "../../utils/validateEmail";
 const router = Router();
 
 router
-  .route('/')
+  .route('/signup')
   .post(async (req, res) => {
     const email = req.body.email;
     const password = req.body.password;
@@ -15,16 +15,57 @@ router
       if(!validateEmail(email)) {
         return res.status(409).send({message: 'Wrong email format'}).end();
       }
-      const user = await User.findOne({email: email});
-      if(user) {
-        return res.status(409).send({message: 'User with that email alredy exist.'}).end();
-      }
       try {
+        const user = await User.findOne({email: email});
+        if(user) {
+          return res.status(409).send({message: 'User with that email alredy exist.'}).end();
+        }
         const hasedPassword = await bcryptjs.hash(password, 10);
         const newUser = await User.create({email, password: hasedPassword});
-        res.status(201).send({newUser, message: 'new user created'}).end();
+        return res
+          .status(201)
+          .send({
+            newUser: {
+              _id: newUser._id,
+              email: newUser.email
+            },
+            message: 'New user created succesfully.'
+          })
+          .end();
       } catch (err) {
-        console.log(err);
+        console.error(err);
+      }
+    } else {
+      res.status(400).send({message: 'email or password missing in request.'}).end();
+    }
+  });
+
+router
+  .route('/login')
+  .post(async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    if(email && password) {
+      try {
+        const user = await User.findOne({email: email});
+        const passwordMatch = await bcryptjs.compare(password, user.password);
+        if(user && passwordMatch) {
+          return res
+            .status(200)
+            .send({
+              user: {
+                _id: user._id,
+                email: user.email
+              },
+              token: 'TO DO',
+              message: 'Login succesfull.'
+            })
+            .end();
+        } else {
+          res.status(401).send({message: 'Authentication failed.'}).end();
+        }
+      } catch (err) {
+        console.error(err);
       }
     } else {
       res.status(400).send({message: 'email or password missing in request.'}).end();

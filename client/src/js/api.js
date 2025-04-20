5eb2fd83ccff815ad1639f7605a886409d3fcd1b
Czapa <lukaszmodelski90@gmail.com>
2025-04-20 20:39:41 +0200

    switched to Render service

--- client/src/js/api.js
+++ client/src/js/api.js
@@ -1,48 +1,54 @@
-import axios from 'axios';
+import axios from "axios";
 import { getJWT } from "./jwt";
 
-const domain = 'https://nodejs-expenses.herokuapp.com/';
+const domain = "https://expenses-sxp4.onrender.com/";
+// const domain = 'https://nodejs-expenses.herokuapp.com/';
+//expenses-sxp4.onrender.com/
 // const domain = 'http://localhost:3000/';
 
 export const handleAPIerror = (error, navigation) => {
   if (error.response && error.response.status === 401) {
-    navigation.navigate('LoginScreen');
-    console.info('Redirecting to login page');
+    navigation.navigate("LoginScreen");
+    console.info("Redirecting to login page");
   } else {
     console.error(error);
   }
-}
+};
 
 export const loginUser = (email, password) => {
   return axios.post(`${domain}api/user/login`, { email, password });
-}
+};
 
 export const fetchExpenses = async () => {
   const jwt = await getJWT();
   return axios.get(`${domain}api/expense`, {
-    params: { jwt: jwt }
+    params: { jwt: jwt },
   });
-}
+};
 
-export const postNewExpense = async (amount, category, description, dateString) => {
-  return axios.post(`${domain}api/expense`,
+export const postNewExpense = async (
+  amount,
+  category,
+  description,
+  dateString
+) => {
+  return axios.post(
+    `${domain}api/expense`,
     {
       amount,
       category,
       description,
-      dateString
+      dateString,
     },
-    { 
-      params: { jwt: await getJWT() }
+    {
+      params: { jwt: await getJWT() },
     }
   );
-}
+};
 
 export const deleteExpenseApi = async (id) => {
-  return axios.delete(`${domain}api/expense`,
-    {
-      data: { id },
-      params: { jwt: await getJWT() }
-    }
-  );
-}
+  return axios.delete(`${domain}api/expense`, {
+    data: { id },
+    params: { jwt: await getJWT() },
+  });
+};
